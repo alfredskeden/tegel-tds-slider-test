da0125bc179b680519c4a0cdb6a546c0efc6d76b
alfredskeden <alfred.skedeback91@gmail.com>
2023-12-05 13:47:45 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewComponent } from './new/new.component';
+import { SliderComponent } from './slider/slider.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'new', component: NewComponent },
+  { path: 'slider', component: SliderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
